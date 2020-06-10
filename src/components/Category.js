2592937eb1c0c50e74209e458485282fcb0d495d
wongprom <jimmybackstrom@hotmail.com>
2020-06-11 00:25:20 +0200

    Added functionality to toggle category on click.

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Category = ({ categorys }) => {
   console.log(categorys);
+  let [showCategory, setShowCategory] = useState(false);
 
   return (
-    <div>
+    <div
+      style={{ border: '1px solid blue' }}
+      onClick={() => setShowCategory((showCategory = !showCategory))}
+    >
       <h2>{categorys.name}</h2>
-      {categorys.products.map((product) => {
+      {showCategory &&
+        categorys.products.map((product) => {
+          return (
+            <div>
+              <p> Rätt: {product.name}</p>
+              <p>{product.price} Kr </p>
+            </div>
+          );
+        })}
+
+      {/* {categorys.products.map((product) => {
         return (
           <div>
             <p> Rätt: {product.name}</p>
             <p>{product.price} Kr </p>
           </div>
         );
-      })}
+      })} */}
     </div>
   );
 };
